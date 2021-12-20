39ff5f7b959d48666146837bbde6f90db22a4ffd
Vaganjm <67558233+Vaganjm@users.noreply.github.com>
2021-12-20 14:50:59 -0500

    Update general.py

--- WARDEN_DEV_CODE/cogs/general.py
+++ WARDEN_DEV_CODE/cogs/general.py
@@ -1,10 +1,4 @@
-""""
-Copyright © Krypton 2021 - https://github.com/kkrypt0nn (https://krypt0n.co.uk)
-Description:
-This is a template to create your own discord bot in python.
 
-Version: 4.0.1
-"""
 
 import json
 import os
